cb2bf85cfb3dc257dace7d4d53386e21d9d6f5e5
Hasindu30 <wohchandeepa@std.appsc.sab.ac.lk>
2024-05-08 14:06:13 +0530

    update home page

--- Brandspark/src/App.jsx
+++ Brandspark/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './Components/Navbar/Navbar'
 import Signup from './Pages/Signup'
 import Login from './Pages/Login'
 import Home from './Pages/Home/Home'
+import  Expertsignup from './Pages/Expertsignup'
 
 import Footer from './Components/Footer'
 
--- Brandspark/src/Pages/Home/Home.jsx
+++ Brandspark/src/Pages/Home/Home.jsx
@@ -6,6 +6,10 @@ import images from '../Home/data/images'
 import CustomSlider from '../Home/slider/Custom.slider'
 import logomockup from '../../assets/logomockups.png'
 import logomakerhome from '../../assets/logomakerhome.png'
+import brandhome from '../../assets/brandhome.png'
+import homecontact from '../../assets/homecontact.png'
+import homeexpert from '../../assets/homeexpert.png'
+
 
 
 
@@ -56,18 +60,43 @@ const Home = () => {
         <div className="logomaker-home">
       
         <div className="homeleft">
-        <h1>Create your own brand </h1>
-          <p>To get started with Brandspark's logo maker just enter your business name and let 
-          our logo creator quickly creates outstanding logo designs. You can search through thousands 
-          of custom logos made with your company name to find the ideal one. Do you need a logobr
-           that is more precise? 
-          Not a problem! To narrow down the logos and find a design that truly works for your company, you may enter keywords.
+        <h1>Create your own brand and promote </h1>
+          <p>Welcome to Brandspark, where we sculpt your vision into reality. 
+            Our mission is to empower individuals and businesses to create their own distinct identities. 
+            Join us in crafting your brand story and standing out in the crowded market. Let's embark on this journey of creativity and innovation together
           </p>
           <button className='getstart'>Get start</button>
           
         </div>
-        <img src={logomakerhome} alt=""/>
+        <img src={brandhome} alt=""/>
         </div>
+
+        <div className="logomaker-home">
+      <img src={homecontact} alt=""/>
+        <div className="homeleft">
+        <h1>Contact to Brandspark to your business</h1>
+          <p>To get start with Brandspark AI-powered platform 
+            to promote your own brand and bring your brand to life contact us and visit 
+            our Ridiculous packages.You can get all the support you need to develop your business 
+            through Brandspark.Contact us today without delay.
+          </p>
+          <button className='getstart'>Get start</button>
+        </div>
+        </div>
+
+        <div className="logomaker-home">
+      
+        <div className="homeleft">
+        <h1>Analyze your business with Brandspark's experts</h1>
+          <p>Whether you're looking to optimize your marketing strategies, enhance your brand identity, or streamline your operations, our experts are here to help. By leveraging years of industry experience and cutting-edge analytical tools, we provide actionable recommendations tailored to your specific needs.
+        
+Ready to take your business to the next level? Get in touch with our experts today to schedule a consultation.
+          </p>
+          <button className='getstart'>Get start</button>
+        </div>
+        <img src={homeexpert} alt=""/>
+        </div>
+        
       
     </div>
     
