d64d8e70ae4102a796a48610eee8487d40f11208
Hasindu30 <wohchandeepa@std.appsc.sab.ac.lk>
2024-05-11 16:56:07 +0530

    Home Page Update

--- Brandspark/src/App.jsx
+++ Brandspark/src/App.jsx
@@ -4,10 +4,7 @@ import Signup from './Pages/Signup'
 import Login from './Pages/Login'
 import Home from './Pages/Home/Home'
 import  Expertsignup from './Pages/Expertsignup'
-
 import Footer from './Components/Footer'
-
-
 import CreateLogo from './Pages/CreateLogo/CreateLogo'
 import Expert from './Pages/Expert/Expert'
 
@@ -17,8 +14,9 @@ const App = () => {
   
   return (
     <div>
-
+      
      <Home/>
+     
       
       
     </div>
--- Brandspark/src/Components/Navbar/Navbar.jsx
+++ Brandspark/src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Link} from 'react-router-dom'
 import './Navbar.css'
 import logo from '../../assets/logo2.png'
+import menures from '../../assets/menures.png'
 
 const Navbar = () => {
+  const [mobileMenu,setMobileMenu] =useState(false);
+  const toggleMenu =()=>{
+    mobileMenu? setMobileMenu(false) : setMobileMenu(true);
+  }
   return (
     <nav className='navbar'>
          <img src={logo} alt="" className='logo'/> 
-         
+          
             <ul className='navbar-mid'>
                 <li>Home</li>
                 <li>Logo maker</li>
@@ -21,7 +26,17 @@ const Navbar = () => {
             <li className='btnnav'>Signup</li>
             </ul>
             
-            
+          <ul className={mobileMenu ? 'response' : 'hide-mobile-menu'}>
+
+                <li>Home</li>
+                <li>Logo maker</li>
+                <li>Brand kit</li>
+                <li>Contact</li>
+                <li>Experts</li>
+                <li>Login</li>
+                <li>Signup</li>
+          </ul>
+          <img src={menures} alt="" className='menures' onClick={toggleMenu}/>
         </nav>
   )
 }
