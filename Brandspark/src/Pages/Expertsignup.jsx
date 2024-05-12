53b46eb0891e48118127a6fd785e7b1058d5b994
NimeshMadhuwantha <janmadhuwantha@std.appsc.sab.ac.lk>
2024-05-12 23:06:01 +0530

    update expertsignup

--- Brandspark/src/Pages/Expertsignup.jsx
+++ Brandspark/src/Pages/Expertsignup.jsx
@@ -8,14 +8,14 @@ const Expertsignup = () => {
 
 <div className="wrapper">
   <form action="#">
-    <h2>Sign Up</h2>
+    <h2>Expert Sign Up</h2>
     <div className="input-field">
       <input type="text" required />
-      <label htmlFor="First Name">Enter your First Name</label>
+      <label htmlFor="First Name">Enter your Full Name</label>
     </div>
     <div className="input-field">
       <input type="text" required />
-      <label htmlFor="Second Name">Enter your Second Name</label>
+      <label htmlFor="Second Name">Enter Occupation</label>
     </div>
     <div className="input-field">
       <input type="text" required />
@@ -32,7 +32,7 @@ const Expertsignup = () => {
 
     <button type="submit">Sign Up</button>
     <div className="Login"><br/>
-      <p className='Log'>Don't have an account? <a href="/Login">Register</a></p>
+      <p className='Log'>If have an account? <a href="/Login">Login</a></p>
     </div>
   </form>
 
