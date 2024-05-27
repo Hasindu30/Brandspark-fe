c2b78023a954cbd7038ef25887a4774b034c6197
nethhari <chemininethhari@gmail.com>
2024-05-27 09:46:20 +0530

    Finished Brand Kit page

---
+++ Brandspark/src/Pages/BrandKit/MarketingTools/MT.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import './MT.css';
+import mockup1 from '../../../assets/BrandKit/1.png';
+import mockup2 from '../../../assets/BrandKit/2.png';
+import mockup3 from '../../../assets/BrandKit/3.jpg';
+import mockup4 from '../../../assets/BrandKit/4.jpg';
+import mockup5 from '../../../assets/BrandKit/5.png';
+import mockup6 from '../../../assets/BrandKit/6.png';
+import mockup7 from '../../../assets/BrandKit/7.png';
+import mockup8 from '../../../assets/BrandKit/8.jpg';
+import mockup9 from '../../../assets/BrandKit/9.jpg';
+
+const MT = () => {
+    const items = [
+        { id: 1, title: 'Social Profiles', image: mockup1 },
+        { id: 2, title: 'Social Post', image: mockup2 },
+        { id: 3, title: 'Business Cards', image: mockup3 },
+        { id: 4, title: 'Letter Heads', image: mockup4 },
+        { id: 5, title: 'Posters', image: mockup5 },
+        { id: 6, title: 'Presentations', image: mockup6 },
+        { id: 7, title: 'Invitations', image: mockup7 },
+        { id: 8, title: 'Web Sites', image: mockup8 },
+        { id: 9, title: 'Brochures', image: mockup9 }
+    ];
+
+    return (
+        <div className="brandkit-container">
+            <div className="brandkit-grid">
+                {items.map(item => (
+                    <div key={item.id} className="brandkit-item">
+                        <img src={item.image} alt={item.title} className="brandkit-image" />
+                        <div className="overlay">
+                            <div className="overlay-text">{item.title}</div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default MT;
