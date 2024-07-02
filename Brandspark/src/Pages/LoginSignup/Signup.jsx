aa85cfdd82d57521fa658864e65c6f6aec17c5ed
Hasindu30 <wohchandeepa@std.appsc.sab.ac.lk>
2024-07-02 15:21:07 +0530

    add google oauth login yto login page

--- Brandspark/src/Pages/LoginSignup/Signup.jsx
+++ Brandspark/src/Pages/LoginSignup/Signup.jsx
@@ -1,50 +1,63 @@
-import React from 'react'
+import React from 'react';
 import './Signup.css';
 import Navbar from '../../Components/Navbar/Navbar';
+import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
 
 const Signup = () => {
+  const handleSuccess = (credentialResponse) => {
+    console.log('Login Success:', credentialResponse);
+    // Handle the response and send it to your backend for further processing
+  };
+
+  const handleFailure = () => {
+    console.log('Login Failed');
+  };
+
   return (
-    <div>
-      <Navbar/>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+      <div>
+        <Navbar/>
         <div>
-
-<div className="wrapper">
-  
-      <form action="#">
-        <h2>Sign Up</h2>
-        <div className="input-field">
-          <input type="text" required />
-          <label htmlFor="First Name">Enter your First Name</label>
+          <div className="wrapper">
+            <form action="#">
+              <h2>Sign Up</h2>
+              <div className="input-field">
+                <input type="text" required />
+                <label htmlFor="First Name">Enter your First Name</label>
+              </div>
+              <div className="input-field">
+                <input type="text" required />
+                <label htmlFor="Second Name">Enter your Second Name</label>
+              </div>
+              <div className="input-field">
+                <input type="text" required />
+                <label htmlFor="email">Enter your email</label>
+              </div>
+              <div className="input-field">
+                <input type="password" required />
+                <label htmlFor="password">Enter your password</label>
+              </div>
+              <div className="input-field">
+                <input type="password" required />
+                <label htmlFor="confirmPassword">Confirm password</label>
+              </div>
+              <br/><br/>
+              <button type="submit">Signup</button>
+              <div className="Login"><br/>
+                <p className='Log'>If have an account? <a href="/Login">Login</a></p>
+              </div>
+            </form>
+            <div className="google-login">
+              <GoogleLogin
+                onSuccess={handleSuccess}
+                onFailure={handleFailure}
+              />
+            </div>
+          </div>
         </div>
-        <div className="input-field">
-          <input type="text" required />
-          <label htmlFor="Second Name">Enter your Second Name</label>
-        </div>
-        <div className="input-field">
-          <input type="text" required />
-          <label htmlFor="email">Enter your email</label>
-        </div>
-        <div className="input-field">
-          <input type="password" required />
-          <label htmlFor="password">Enter your password</label>
-        </div>
-        <div className="input-field">
-          <input type="confirmPassword" required />
-          <label htmlFor="confirmPassword">Confirm password</label>
-        </div><br/><br/>
-
-        <button type="submit">Signup</button>
-        <div className="Login"><br/>
-          <p className='Log'>If have an account? <a href="/Login">Login</a></p>
-        </div>
-      </form>
-
-    </div>
-    </div>
-
-
-    </div>
+      </div>
+    </GoogleOAuthProvider>
   )
 }
 
-export default Signup
+export default Signup;
