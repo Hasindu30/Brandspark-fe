bf90accc9ff534a65a72769006dd8d92a965acab
Hasindu30 <wohchandeepa@std.appsc.sab.ac.lk>
2024-07-02 16:28:19 +0530

    add google oauth login yto login page

--- Brandspark/src/Pages/LoginSignup/Login.jsx
+++ Brandspark/src/Pages/LoginSignup/Login.jsx
@@ -53,8 +53,8 @@ const Login = () => {
                   <img
                     src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png"
                     alt="Google"
-                    width="20"
-                    height="20"
+                    width="200"
+                    height="200"
                   />
                   Login with Google
                 </button>
